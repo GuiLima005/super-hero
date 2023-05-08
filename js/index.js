cf224ea4fc58a3a9f540722633be7bc7d9755ebe
GUILHERME DE OLIVEIRA LIMA <22282086@CFP127ALUNO.LOCAL>
2023-05-08 08:50:11 -0300

    melhoria no lighthouse

--- js/index.js
+++ js/index.js
@@ -51,6 +51,8 @@ const fetchAllSuperHero = async (buscaTexto) => {
 const showSearchList = (data) => {
 
     searchList.innerHTML = ""
+    console.log(data);
+    
 
     data.forEach(dataItem => {
         const divElem = document.createElement('div')
@@ -98,13 +100,13 @@ searchList.addEventListener('click', (event) => {
 
     searchList.innerHTML = ""
 
-    console.log(searchId)
+    // console.log(searchId)
 })
 
 // Função para exibir as informações na tela.
 const showSuperheroDetails = (data) => {
 
-    console.log(data)
+    // console.log(data)
 
     document.querySelector('.personagem').innerHTML = `
         <img class = "heroi" src = "${data.image.url}">
